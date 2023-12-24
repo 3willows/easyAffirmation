261c1394532297c95e31ae85da6acde7e8861ed5
Jasper Wong <wlcjasper@gmail.com>
2023-12-24 21:30:10 +0800

    add buttons

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+// automatically generate paragraph boxes
+
 document.addEventListener('DOMContentLoaded', function () {
-  const affirmationContent = document.querySelector('.affirmationContent');
-  const addButton = document.getElementById('addParagraph');
-  let paragraphNumber = 1;
+  const affirmationContent = document.querySelector('.affirmationContent')
+  const addButton = document.getElementById('addParagraph')
+  let paragraphNumber = 1
 
   addButton.addEventListener('click', function () {
-    paragraphNumber++;
-    const newTextArea = document.createElement('textarea');
-    newTextArea.setAttribute('rows', '4');
-    newTextArea.setAttribute('cols', '50');
-    newTextArea.id = `paragraph${paragraphNumber}`;
+    paragraphNumber++
+    const newTextArea = document.createElement('textarea')
+    newTextArea.setAttribute('rows', '4')
+    newTextArea.setAttribute('cols', '50')
+    newTextArea.id = `paragraph${paragraphNumber}`
 
-    const paragraph = document.createElement('p');
-    paragraph.textContent = `${paragraphNumber}. `;
-    paragraph.appendChild(newTextArea);
-    affirmationContent.insertBefore(paragraph, addButton);
-  });
-});
+    const paragraph = document.createElement('p')
+    paragraph.textContent = `${paragraphNumber}. `
+    paragraph.appendChild(newTextArea)
+    affirmationContent.insertBefore(paragraph, addButton)
+  })
+})
+
